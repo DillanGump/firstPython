b7168d5e284fe1bea1611cd91b3b429fdde5fab8
Dillan Gump <gump.31@buckeyemail.osu.edu>
2020-04-10 23:25:57 -0400

    A second attempt to generate a pull request on git hub

--- basics.py
+++ basics.py
@@ -1,3 +1,4 @@
 import datetime
 print(datetime.datetime.now())
-print("Look, a change!")
+# print("Look, a change!") maybe this time, V-
+print("Exploring versioning with branches. Let's see if this makes a pull request!")
